ec300eb156934ef649729154de8c6f8bf493deb1
Rory Primrose <rory@neovolve.com>
2016-05-05 08:48:43 +1000

    Aligned IRandomGenerator.Next method names with IRandomGenerator.NextValue signatures. (#4)
    Moved IRandomGenerator generic signature to extension method.
    Added support for the decimal type.

--- ModelBuilder.UnitTests/ScenarioTests.cs
+++ ModelBuilder.UnitTests/ScenarioTests.cs
@@ -29,6 +29,23 @@ namespace ModelBuilder.UnitTests
         }
 
         [Fact]
+        public void CanCreateAllNumberTypesTest()
+        {
+            var actual = Model.Create<Numbers>();
+
+            actual.First.Should().NotBe(0);
+            actual.Second.Should().NotBe(0);
+            actual.Third.Should().NotBe(0);
+            actual.Fourth.Should().NotBe(0);
+            actual.Fifth.Should().NotBe(0);
+            actual.Sixth.Should().NotBe(0);
+            actual.Seventh.Should().NotBe(0);
+            actual.Eighth.Should().NotBe(0);
+            actual.Nineth.Should().NotBe(0);
+            actual.Tenth.Should().NotBe(0);
+        }
+
+        [Fact]
         public void CreateBuildsAndPopulatesNestedInstancesTest()
         {
             var actual = Model.Create<Person>();
