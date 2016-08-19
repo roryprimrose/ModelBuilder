3eaa58c0fc22208b8542e1cb76c626ea8b3ca0b9
Rory Primrose <rory@neovolve.com>
2016-08-19 15:55:49 +1000

    Feature typecreation (#16)
    * Added DefaultExecuteStrategy as a super class of DefaultExecutionStrategy<T> and moved the bulk of logic down.
    
    * Added support for creating instances specifying Type as well as <T>.
    
    Closes #15

--- ModelBuilder/ExecuteStrategyExtensions.cs
+++ ModelBuilder/ExecuteStrategyExtensions.cs
@@ -3,18 +3,18 @@ namespace ModelBuilder
     using System;
 
     /// <summary>
-    /// The <see cref="Extensions"/>
-    /// class provides extension methods for the <see cref="IExecuteStrategy{T}"/> interface.
+    ///     The <see cref="Extensions" />
+    ///     class provides extension methods for the <see cref="IExecuteStrategy{T}" /> interface.
     /// </summary>
     public static class ExecuteStrategyExtensions
     {
         /// <summary>
-        /// Creates an instance of <typeparamref name="T"/> using the specified execute strategy.
+        ///     Creates an instance of <typeparamref name="T" /> using the specified execute strategy.
         /// </summary>
         /// <typeparam name="T">The type of instance to create.</typeparam>
         /// <param name="executeStrategy">The execute strategy to create the instance with.</param>
         /// <returns>The new instance.</returns>
-        /// <exception cref="ArgumentNullException">The <paramref name="executeStrategy"/> parameter is null.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="executeStrategy" /> parameter is null.</exception>
         public static T Create<T>(this IExecuteStrategy<T> executeStrategy)
         {
             if (executeStrategy == null)
@@ -23,6 +23,29 @@ namespace ModelBuilder
             }
 
             return executeStrategy.CreateWith();
+        }
+
+        /// <summary>
+        ///     Creates an instance of a type using the specified execute strategy.
+        /// </summary>
+        /// <param name="executeStrategy">The execute strategy to create the instance with.</param>
+        /// <param name="instanceType">The type of instance to create.</param>
+        /// <returns>The new instance.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="executeStrategy" /> parameter is null.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="instanceType" /> parameter is null.</exception>
+        public static object Create(this IExecuteStrategy executeStrategy, Type instanceType)
+        {
+            if (executeStrategy == null)
+            {
+                throw new ArgumentNullException(nameof(executeStrategy));
+            }
+
+            if (instanceType == null)
+            {
+                throw new ArgumentNullException(nameof(instanceType));
+            }
+
+            return executeStrategy.CreateWith(instanceType);
         }
     }
 }
