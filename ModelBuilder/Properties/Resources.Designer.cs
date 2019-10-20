33029598446c8ceceee86632b7d2e57e080477c5
Rory Primrose <rory@neovolve.com>
2019-10-20 22:32:03 +1100

    Removed net452

--- ModelBuilder/Properties/Resources.Designer.cs
+++ ModelBuilder/Properties/Resources.Designer.cs
@@ -20,7 +20,7 @@ namespace ModelBuilder.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -229,6 +229,15 @@ namespace ModelBuilder.Properties {
         internal static string DefaultBuildLog_PostBuild {
             get {
                 return ResourceManager.GetString("DefaultBuildLog_PostBuild", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No constructor found matching type..
+        /// </summary>
+        internal static string DefaultTypeCreator_NoMatchingConstructor {
+            get {
+                return ResourceManager.GetString("DefaultTypeCreator_NoMatchingConstructor", resourceCulture);
             }
         }
         
