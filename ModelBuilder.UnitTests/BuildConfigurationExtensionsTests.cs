04581b3719eb0a5635813308c9c779446368fd5a
Rory Primrose <rory@neovolve.com>
2020-01-16 20:10:33 +1100

    Moved extension methods from BuildStrategyExtensions to BuildConfigurationExtensions

--- ModelBuilder.UnitTests/BuildConfigurationExtensionsTests.cs
+++ ModelBuilder.UnitTests/BuildConfigurationExtensionsTests.cs
@@ -1,8 +1,14 @@
 ﻿namespace ModelBuilder.UnitTests
 {
     using System;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
     using FluentAssertions;
+    using ModelBuilder.TypeCreators;
     using ModelBuilder.UnitTests.Models;
+    using ModelBuilder.ValueGenerators;
+    using NSubstitute;
     using Xunit;
 
     public class BuildConfigurationExtensionsTests
@@ -29,6 +35,209 @@
         public void CloneThrowsExceptionWithNullBuildStrategyTest()
         {
             Action action = () => ((IBuildStrategy) null).Clone();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+
+        [Fact]
+        public void CreateReturnsInstanceCreatedByDefaultExecuteStrategyTest()
+        {
+            var value = Guid.NewGuid();
+
+            var generator = Substitute.For<IValueGenerator>();
+            var generators = new Collection<IValueGenerator>
+            {
+                generator
+            };
+            var target = Substitute.For<IBuildConfiguration>();
+
+            target.ValueGenerators.Returns(generators);
+            generator.IsSupported(typeof(Guid), null, Arg.Any<IBuildChain>()).Returns(true);
+            generator.Generate(typeof(Guid), null, Arg.Any<IExecuteStrategy>()).Returns(value);
+
+            var actual = target.Create(typeof(Guid));
+
+            actual.Should().Be(value);
+        }
+
+        [Fact]
+        public void CreateThrowsExceptionWithNullInstanceTypeTest()
+        {
+            var target = Substitute.For<IBuildConfiguration>();
+
+            Action action = () => target.Create(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void CreateThrowsExceptionWithNullStrategyTest()
+        {
+            Action action = () => ((IBuildConfiguration) null).Create(typeof(Guid));
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void CreateTReturnsInstanceCreatedByDefaultExecuteStrategyTest()
+        {
+            var value = Guid.NewGuid();
+
+            var generator = Substitute.For<IValueGenerator>();
+            var generators = new Collection<IValueGenerator>
+            {
+                generator
+            };
+            var target = Substitute.For<IBuildConfiguration>();
+
+            target.ValueGenerators.Returns(generators);
+            generator.IsSupported(typeof(Guid), null, Arg.Any<IBuildChain>()).Returns(true);
+            generator.Generate(typeof(Guid), null, Arg.Any<IExecuteStrategy>()).Returns(value);
+
+            var actual = target.Create<Guid>();
+
+            actual.Should().Be(value);
+        }
+
+        [Fact]
+        public void CreateTThrowsExceptionWithNullStrategyTest()
+        {
+            Action action = () => ((IBuildConfiguration) null).Create<Guid>();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void IgnoringReturnsNewBuildConfigurationWithIgnoreRuleAppendedTest()
+        {
+            var ignoreRules = new Collection<IgnoreRule>();
+
+            var target = Substitute.For<IBuildConfiguration>();
+
+            target.IgnoreRules.Returns(ignoreRules);
+
+            var actual = target.Ignoring<Person>(x => x.Priority);
+
+            actual.IgnoreRules.Should().NotBeEmpty();
+
+            var matchingRule =
+                actual.IgnoreRules.FirstOrDefault(x => x.PropertyName == "Priority" && x.TargetType == typeof(Person));
+
+            matchingRule.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void IgnoringThrowsExceptionWithNullExpressionTest()
+        {
+            var target = Substitute.For<IBuildConfiguration>();
+
+            Action action = () => target.Ignoring<Person>(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void IgnoringThrowsExceptionWithNullStrategyTest()
+        {
+            Action action = () => ((IBuildConfiguration) null).Ignoring<Person>(x => x.Priority);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void MappingReturnsNewBuildConfigurationWithTypeMappingRuleAppendedTest()
+        {
+            var typeMappings = new Collection<TypeMappingRule>();
+
+            var target = Substitute.For<IBuildConfiguration>();
+
+            target.TypeMappingRules.Returns(typeMappings);
+
+            var actual = target.Mapping<Stream, MemoryStream>();
+
+            actual.TypeMappingRules.Should().NotBeEmpty();
+
+            var matchingRule = actual.TypeMappingRules.FirstOrDefault(
+                x => x.SourceType == typeof(Stream) && x.TargetType == typeof(MemoryStream));
+
+            matchingRule.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void MappingThrowsExceptionWithNullStrategyTest()
+        {
+            Action action = () => ((IBuildConfiguration) null).Mapping<Stream, MemoryStream>();
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void PopulateThrowsExceptionWithNullBuildConfigurationTest()
+        {
+            var model = new Person();
+
+            Action action = () => ((IBuildConfiguration) null).Populate(model);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void PopulateThrowsExceptionWithNullInstanceTest()
+        {
+            var target = Substitute.For<IBuildConfiguration>();
+
+            Action action = () => target.Populate<Person>(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void PopulateUsesDefaultExecuteStrategyToPopulateInstanceTest()
+        {
+            var value = Guid.NewGuid();
+            var expected = new SlimModel();
+
+            var target = Substitute.For<IBuildConfiguration>();
+            var creator = Substitute.For<ITypeCreator>();
+            var creators = new Collection<ITypeCreator>
+            {
+                creator
+            };
+            var generator = Substitute.For<IValueGenerator>();
+            var generators = new Collection<IValueGenerator>
+            {
+                generator
+            };
+
+            target.TypeCreators.Returns(creators);
+            target.ValueGenerators.Returns(generators);
+            creator.CanPopulate(typeof(SlimModel), null, Arg.Any<IBuildChain>()).Returns(true);
+            creator.Populate(expected, Arg.Any<IExecuteStrategy>()).Returns(expected);
+            generator.IsSupported(typeof(Guid), "Value", Arg.Is<IBuildChain>(x => x.Last == expected)).Returns(true);
+            generator.Generate(typeof(Guid), "Value", Arg.Is<IExecuteStrategy>(x => x.BuildChain.Last == expected))
+                .Returns(value);
+
+            var actual = target.Populate(expected);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void UsingExecuteStrategyReturnsExecuteStrategyWithBuildConfigurationTest()
+        {
+            var target = Substitute.For<IBuildConfiguration>();
+
+            var actual = target.UsingExecuteStrategy<DefaultExecuteStrategy<Person>>();
+
+            actual.Configuration.Should().BeSameAs(target);
+            actual.Log.Should().BeOfType<DefaultBuildLog>();
+        }
+
+        [Fact]
+        public void UsingExecuteStrategyThrowsExceptionWithNullConfigurationTest()
+        {
+            Action action = () => ((IBuildConfiguration) null).UsingExecuteStrategy<NullExecuteStrategy>();
 
             action.Should().Throw<ArgumentNullException>();
         }
--- ModelBuilder/BuildConfigurationExtensions.cs
+++ ModelBuilder/BuildConfigurationExtensions.cs
@@ -1,9 +1,10 @@
 ﻿namespace ModelBuilder
 {
     using System;
+    using System.Linq.Expressions;
 
     /// <summary>
-    ///     The <see cref="Extensions" />
+    ///     The <see cref="BuildConfigurationExtensions" />
     ///     class provides extension methods for the <see cref="IBuildConfiguration" /> interface.
     /// </summary>
     public static class BuildConfigurationExtensions
@@ -58,6 +59,156 @@
             }
 
             return compiler;
+        }
+
+        /// <summary>
+        ///     Creates an instance of <typeparamref name="T" /> using the specified build configuration and any provided
+        ///     constructor
+        ///     arguments.
+        /// </summary>
+        /// <typeparam name="T">The type of instance to create.</typeparam>
+        /// <param name="buildConfiguration">The build configuration to create the instance with.</param>
+        /// <param name="args">The constructor arguments to create the type with.</param>
+        /// <returns>The new instance.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="buildConfiguration" /> parameter is <c>null</c>.</exception>
+        /// <remarks>This method uses <see cref="DefaultExecuteStrategy{T}" /> to create the instance.</remarks>
+        public static T Create<T>(this IBuildConfiguration buildConfiguration, params object[] args)
+        {
+            if (buildConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(buildConfiguration));
+            }
+
+            return buildConfiguration.UsingExecuteStrategy<DefaultExecuteStrategy<T>>().Create(args);
+        }
+
+        /// <summary>
+        ///     Creates an instance of a type using the specified build configuration and any provided constructor arguments.
+        /// </summary>
+        /// <param name="buildConfiguration">The build configuration to create the instance with.</param>
+        /// <param name="instanceType">The type of instance to create.</param>
+        /// <param name="args">The constructor arguments to create the type with.</param>
+        /// <returns>The new instance.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="buildConfiguration" /> parameter is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="instanceType" /> parameter is <c>null</c>.</exception>
+        /// <remarks>This method uses <see cref="DefaultExecuteStrategy" /> to create the instance.</remarks>
+        public static object Create(this IBuildConfiguration buildConfiguration, Type instanceType,
+            params object[] args)
+        {
+            if (buildConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(buildConfiguration));
+            }
+
+            if (instanceType == null)
+            {
+                throw new ArgumentNullException(nameof(instanceType));
+            }
+
+            return buildConfiguration.UsingExecuteStrategy<DefaultExecuteStrategy>().Create(instanceType, args);
+        }
+
+        /// <summary>
+        ///     Appends a new <see cref="IgnoreRule" /> to the build configuration using the specified
+        ///     expression.
+        /// </summary>
+        /// <typeparam name="T">The type of instance that matches the rule.</typeparam>
+        /// <param name="buildConfiguration">The build configuration to update.</param>
+        /// <param name="expression">The expression that identifies a property on <typeparamref name="T" /></param>
+        /// <returns>The build configuration with the new rule.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="buildConfiguration" /> parameter is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="expression" /> parameter is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">The <paramref name="expression" /> parameter does not represent a property.</exception>
+        /// <exception cref="ArgumentException">
+        ///     The <paramref name="expression" /> parameter does not match a property on the type
+        ///     to generate.
+        /// </exception>
+        public static IBuildConfiguration Ignoring<T>(
+            this IBuildConfiguration buildConfiguration,
+            Expression<Func<T, object>> expression)
+        {
+            if (buildConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(buildConfiguration));
+            }
+
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+
+            var targetType = typeof(T);
+            var property = expression.GetProperty();
+
+            var rule = new IgnoreRule(targetType, property.Name);
+
+            return buildConfiguration.Clone().Add(rule).Compile();
+        }
+
+        /// <summary>
+        ///     Appends a new <see cref="TypeMappingRule" /> to the build configuration using the
+        ///     specified types.
+        /// </summary>
+        /// <typeparam name="TSource">The source type to use for type mapping.</typeparam>
+        /// <typeparam name="TTarget">The target type to use for type mapping.</typeparam>
+        /// <param name="buildConfiguration">The build configuration to update.</param>
+        /// <returns>The build configuration with the new rule.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="buildConfiguration" /> parameter is <c>null</c>.</exception>
+        public static IBuildConfiguration Mapping<TSource, TTarget>(this IBuildConfiguration buildConfiguration)
+        {
+            if (buildConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(buildConfiguration));
+            }
+
+            var sourceType = typeof(TSource);
+            var targetType = typeof(TTarget);
+
+            var rule = new TypeMappingRule(sourceType, targetType);
+
+            return buildConfiguration.Clone().Add(rule).Compile();
+        }
+
+        /// <summary>
+        ///     Populates the specified instance using the provided build configuration.
+        /// </summary>
+        /// <typeparam name="T">The type of instance to populate.</typeparam>
+        /// <param name="buildConfiguration">The build configuration to use.</param>
+        /// <param name="instance">The instance to populate.</param>
+        /// <returns>The updated instance.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="buildConfiguration" /> parameter is <c>null</c>.</exception>
+        public static T Populate<T>(this IBuildConfiguration buildConfiguration, T instance)
+        {
+            if (buildConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(buildConfiguration));
+            }
+
+            return buildConfiguration.UsingExecuteStrategy<DefaultExecuteStrategy<T>>().Populate(instance);
+        }
+
+        /// <summary>
+        ///     Returns a new <see cref="IExecuteStrategy{T}" /> for the specified build configuration.
+        /// </summary>
+        /// <typeparam name="T">The type of <see cref="IExecuteStrategy{T}" /> to return.</typeparam>
+        /// <param name="buildConfiguration">The build configuration.</param>
+        /// <returns>A new execute strategy.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="buildConfiguration" /> parameter is <c>null</c>.</exception>
+        public static T UsingExecuteStrategy<T>(this IBuildConfiguration buildConfiguration)
+            where T : IExecuteStrategy, new()
+        {
+            if (buildConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(buildConfiguration));
+            }
+
+            var buildLog = new DefaultBuildLog();
+
+            var executeStrategy = new T();
+
+            executeStrategy.Initialize(buildConfiguration, buildLog);
+
+            return executeStrategy;
         }
     }
 }
