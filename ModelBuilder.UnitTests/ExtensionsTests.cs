f1046a4bf6a8f1a0f628628cc4e2cb14f0f12e60
Andy Berman <bermo@users.noreply.github.com>
2021-08-29 16:01:54 +1000

    Added Set extension method that supports setting properties with inaccessible setters (#202)
    * Support for setting properties with inaccessible setters
    * Added null parameter unit tests
    
    Co-authored-by: Andy <andrew.berman@rxpservices.com>

--- ModelBuilder.UnitTests/ExtensionsTests.cs
+++ ModelBuilder.UnitTests/ExtensionsTests.cs
@@ -100,5 +100,186 @@
 
             action.Should().Throw<ArgumentNullException>();
         }
+
+        [Fact]
+        public void SetExpressionThrowsExceptionWithNullAction()
+        {
+            var sut = new PropertySetters();
+
+            Action action = () => sut.Set(null!, true);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void SetExpressionThrowsExceptionWithNullInstance()
+        {
+            var sut = new PropertySetters();
+
+            Action action = () => ((PropertySetters)null!).Set(x => x.AutoPublic, Guid.Empty);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void SetExpressionAutoPublicSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Guid.NewGuid();
+
+            var actual = sut.Set(x => x.AutoPublic, expected);
+
+            actual.AutoPublic.Should().Be(expected);
+            sut.AutoPublic.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionAutoReadonlySetter()
+        {
+            var sut = new PropertySetters();
+
+            var actual = sut.Set(x => x.AutoReadonly, null);
+
+            actual.AutoReadonly.Should().BeNull();
+            sut.AutoReadonly.Should().BeNull();
+        }
+
+        [Fact]
+        public void SetExpressionAutoPrivateSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<int>();
+
+            var actual = sut.Set(x => x.AutoPrivate, expected);
+
+            actual.AutoPrivate.Should().Be(expected);
+            sut.AutoPrivate.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionAutoProtectedSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<decimal>();
+
+            var actual = sut.Set(x => x.AutoProtected, expected);
+
+            actual.AutoProtected.Should().Be(expected);
+            sut.AutoProtected.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionAutoProtectedInternalSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<Uri>();
+
+            var actual = sut.Set(x => x.AutoProtectedInternal, expected);
+
+            actual.AutoProtectedInternal.Should().BeEquivalentTo(expected);
+            sut.AutoProtectedInternal.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void SetExpressionAutoInternalSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<DateTimeOffset>();
+
+            var actual = sut.Set(x => x.AutoInternal, expected);
+
+            actual.AutoInternal.Should().Be(expected);
+            sut.AutoInternal.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionAutoPrivateInternalSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<PropertySetters>();
+
+            var actual = sut.Set(x => x.AutoPrivateInternal, expected);
+
+            actual.AutoPrivateInternal.Should().BeEquivalentTo(expected);
+            sut.AutoPrivateInternal.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void SetExpressionAutoInitSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<ConsoleColor>();
+
+            var actual = sut.Set(x => x.AutoInit, expected);
+
+            actual.AutoInit.Should().Be(expected);
+            sut.AutoInit.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionBackingField()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<float>();
+
+            var actual = sut.Set(x => x._backingField, expected);
+
+            actual._backingField.Should().Be(expected);
+            sut._backingField.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionPublicBackingFieldSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<float>();
+
+            var actual = sut.Set(x => x.PublicBackingField, expected);
+
+            actual.PublicBackingField.Should().Be(expected);
+            sut.PublicBackingField.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionPrivateBackingFieldSetter()
+        {
+            var sut = new PropertySetters();
+            var expected = Model.Create<float>();
+
+            var actual = sut.Set(x => x.PrivateBackingField, expected);
+
+            actual.PrivateBackingField.Should().Be(expected);
+            sut.PrivateBackingField.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SetExpressionReadonlySetterThrowsException()
+        {
+            var sut = new PropertySetters();
+
+            Action action = () => sut.Set(x => x.Readonly, null);
+
+            action.Should().Throw<NotSupportedException>();
+        }
+
+        [Fact]
+        public void SetExpressionMethodThrowsException()
+        {
+            var sut = new PropertySetters();
+
+            Action action = () => sut.Set(x => x.BackingFieldMethod(), default(float));
+
+            action.Should().Throw<NotSupportedException>();
+        }
+
+        [Fact]
+        public void SetExpressionComplexExpressionThrowsException()
+        {
+            var sut = new PropertySetters();
+
+            Action action = () => sut.Set(x => x.AutoPublic.ToString(), string.Empty);
+
+            action.Should().Throw<NotSupportedException>();
+        }
     }
 }
---
+++ ModelBuilder.UnitTests/Models/PropertySetters.cs
@@ -0,0 +1,33 @@
+﻿namespace ModelBuilder.UnitTests.Models
+{
+    using System;
+
+    public class PropertySetters
+    {
+        public Guid AutoPublic { get; set; }
+
+        public string? AutoReadonly { get; } = string.Empty;
+
+        public int AutoPrivate { get; private set; }
+
+        public decimal AutoProtected { get; protected set; }
+
+        public Uri? AutoProtectedInternal { get; protected internal set; }
+
+        public DateTimeOffset AutoInternal { get; internal set; }
+
+        public PropertySetters? AutoPrivateInternal { get; private protected set; }
+
+        public ConsoleColor AutoInit { get; init; }
+
+        internal float _backingField;
+        public float BackingFieldMethod() => _backingField;
+
+        public float PublicBackingField { get => _backingField; set => _backingField = value; }
+        public float BackingField { get => _backingField; set => _backingField = value; }
+
+        public float PrivateBackingField { get => _backingField; private set => _backingField = value; }
+
+        public char? Readonly => default;
+    }
+}
