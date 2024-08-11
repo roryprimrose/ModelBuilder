7af07514aaced0e13f55a02080fb5fce44681339
Rory Primrose <rory@neovolve.com>
2024-08-11 20:44:50 +1000

    Updated DefaultPropertyResolver.IsIgnored to return true for a property value that references the owning instance (#348)

---
+++ ModelBuilder.UnitTests/Models/SelfReferenceInstance.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelBuilder.UnitTests.Models
+{
+    internal class SelfReferenceInstance
+    {
+        private SelfReferenceInstance? _value;
+
+        public SelfReferenceInstance Instance { get => _value ?? this; set => _value = value; }
+    }
+}
